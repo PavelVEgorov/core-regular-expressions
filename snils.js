31d811d56bb01706c11e977d9be2e7d80167fe9c
Pavel Egorov <egorovpvspb@gmail.com>
2022-05-19 17:15:54 +0300

    Release 1. Другие функции.

--- snils.js
+++ snils.js
@@ -4,33 +4,32 @@
 
 // Определи, есть ли во входной строке номер СНИЛС.
 function hasSNILS(string) {
-  return(/\d{3}-\d{3}-\d{3}\s\d{2}/gi).test(string);
+  return (/\d{3}-\d{3}-\d{3}\s\d{2}/gi).test(string);
 }
-console.log(hasSNILS('The number is 234-600-142 22'))
+
 
 // Найди и верни номер СНИЛС из строки.
 function grabSNILS(string) {
+  return string.match(/\d{3}-\d{3}-\d{3}\s\d{2}/).toString();
 }
-
 
 // Найди и верни все номера СНИЛС, что есть в строке.
 function grabAllSNILS(string) {
-
+  return string.match(/\d{3}-\d{3}-\d{3}\s\d{2}/g);
 }
-
 
 // Зашифруй номера СНИЛС. Example: XXX-XXX-XXX 30.
 function hideAllSNILS(string) {
-
+  return string.replace(/\d{3}-\d{3}-\d{3}\s/g,"XXX-XXX-XXX ")
 }
-
-
 
 // Отформатируй все номера СНИЛС, чтобы использовались правильные разделители:
 // '48001443027', '480.014.430.27', и '480--014--430 27' должны превратиться в '480-014-430 27'.
 function formatSNILS(string) {
-
+  return string.replace(/(\d{3})\D*(\d{3})\D*(\d{3})\D*(\d{2})/g, '$1-$2-$3 $4');
 }
+console.log(formatSNILS('The numbers are 35080207494, 234.600.142.22, and 013--605--876 94'))
+// The numbers are 350-802-074 94, 234-600-142 22, and 013-605-876 94
 
 module.exports = {
   hasSNILS,
